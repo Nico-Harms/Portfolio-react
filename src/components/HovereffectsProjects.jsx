dd7b5e020619e6d2bdf91ca67f82a9a4c5971b63
Nicolai Harms <nicolai.harms@gmail.com>
2023-09-11 22:52:19 +0200

    first commit

---
+++ src/components/HovereffectsProjects.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+
+export default function HovereffectProjects() {
+  const [isPortfolioActive, setPortfolioActive] = useState(false);
+
+  function handlePortfolioHover(e) {
+    const portfolioItem = e.currentTarget;
+    const panel = portfolioItem.querySelector('.portfolio-mask');
+
+    portfolioItem.classList.toggle('active');
+    panel.classList.toggle('show');
+
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
+    const portfolioWrapper = document.querySelector('.portfolio-wrapper');
+    const portfolioList = document.querySelector('.portfolio');
+
+    const hasActivePortfolio = Array.from(portfolioItems).some((item) =>
+      item.classList.contains('active')
+    );
+
+    if (hasActivePortfolio) {
+      portfolioWrapper.classList.add('selected');
+      portfolioList.classList.add('entered');
+    } else {
+      portfolioWrapper.classList.remove('selected');
+      portfolioList.classList.remove('entered');
+    }
+  }
+
+  function handleTextHover(e) {
+    const hpLink = e.currentTarget.getAttribute('data-mask-link');
+    const hpLinks = document.querySelectorAll('.hp-footer h6[data-projectname]');
+    
+    hpLinks.forEach((link) => {
+      if (link.getAttribute('data-projectname') === hpLink) {
+        link.classList.add('hovered');
+      } else {
+        link.classList.remove('hovered');
+      }
+    });
+  }
+
+  function handleTextHoverEnd() {
+    const hpLinks = document.querySelectorAll('.hp-footer h6[data-projectname]');
+    hpLinks.forEach((link) => link.classList.remove('hovered'));
+  }
+
+  return (
+    <div className="portfolio-wrapper">
+      <ul className="portfolio">
+        <div className="project-label">
+          <div className="label-mask-wrap">
+            <h6 className="portfolio-mask"><span>OUR PROJECTS</span></h6>
+            <h6 className="masked">OUR PROJECTS</h6>
+          </div>
+        </div>
+        <li
+          className={`portfolio-item ${isPortfolioActive ? 'active' : ''}`}
+          data-portfolio-service="digital & ecommerce"
+          onMouseEnter={handlePortfolioHover}
+          onMouseLeave={handlePortfolioHover}
+        >
+          <div className="portfolio-mask-wrap">
+            <a href="#">
+              <div className="portfolio-mask"><span>capo</span></div>
+              <span
+                className="fake-link"
+                data-mask-link="capo"
+                onMouseEnter={handleTextHover}
+                onMouseLeave={handleTextHoverEnd}
+              ></span>
+            </a>
+          </div>
+        </li>
+        <li
+          className={`portfolio-item ${isPortfolioActive ? 'active' : ''}`}
+          data-portfolio-service="digital & ecommerce"
+          onMouseEnter={handlePortfolioHover}
+          onMouseLeave={handlePortfolioHover}
+        >
+          <div className="portfolio-mask-wrap">
+            <a href="#">
+              <div className="portfolio-mask"><span>webdoc</span></div>
+              <span
+                className="fake-link"
+                data-mask-link="webdoc"
+                onMouseEnter={handleTextHover}
+                onMouseLeave={handleTextHoverEnd}
+              ></span>
+            </a>
+          </div>
+        </li>
+        <li
+          className={`portfolio-item ${isPortfolioActive ? 'active' : ''}`}
+          data-portfolio-service="digital & brand"
+          onMouseEnter={handlePortfolioHover}
+          onMouseLeave={handlePortfolioHover}
+        >
+          <div className="portfolio-mask-wrap">
+            <a href="#">
+              <div className="portfolio-mask"><span>aarhusvinhandel</span></div>
+              <span
+                className="fake-link"
+                data-mask-link="aarhusvinhandel"
+                onMouseEnter={handleTextHover}
+                onMouseLeave={handleTextHoverEnd}
+              ></span>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,35 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-
 export default function Navigation() {
-  return (
-    <nav>
-      <img className="logo" src="./src/assets/logo.png" alt="" />    
-        <NavLink to="/" end>Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div><img src="" alt="" /></div>
+    <nav className={`navlinks ${isMenuOpen ? "active" : ""}`}>
+      <div className="burger-menu" onClick={toggleMenu}>
+        <div className={`menu__icon ${isMenuOpen ? "active" : ""}`}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <div className="menu-links">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
+      )}
     </nav>
-  )
+    </header>
+  );
 }
