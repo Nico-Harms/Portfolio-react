35a0aa3b5478520983e934b6e840b4c8182b67a9
Nicolai Harms <nicolai.harms@gmail.com>
2023-09-12 14:48:58 +0200

    adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HovereffectProjects from '../components/HovereffectsProjects.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faSquareJs, faReact, faCss3, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faSquareJs, faReact, faCss3Alt, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import BlobSvg from '../components/BlobSvg.jsx';
 
 
@@ -9,24 +9,28 @@ export default function Home() {
   return (
     <div className='siteWrapper'>
       <main className='mainWrapper'>
-        <div className='textToPictureWrapper'>
-          <div className='textWrapperMain'>
-            <h2>Hej, jeg er Nicolai!</h2>
-            <h3>Front-End Developer med passion for inaktive løsniger og design  </h3>
-            <div className='soMe'>
-              <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
-              <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+      <div className='textToPictureWrapper'>
+  <div className='textWrapperMain'>
+  <div className='headerContainer'>
+    <h2>Hej, jeg er Nicolai!</h2>
+   
+      <h3>Front-End Developer med passion for inaktive løsninger og design</h3>
+      <div className='soMe'>
+        <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
+        <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+      </div>
+    </div>
+  </div>
+  <BlobSvg />
+</div>
 
-            </div>
-          </div>
-          <BlobSvg />
-        </div>
         <div className='techStack'>
           <h5 className='techHeader'>Tech Stack</h5>
-          <FontAwesomeIcon icon={faHtml5} />
-          <FontAwesomeIcon icon={faCss3} />
-          <FontAwesomeIcon icon={faSquareJs} />
-          <FontAwesomeIcon icon={faReact} />
+          <hr className='techHr' />
+          <FontAwesomeIcon className='techStackIcon' color='#f06529' icon={faHtml5} />
+          <FontAwesomeIcon className='techStackIcon' color="#2965f1" icon={faCss3Alt} />
+          <FontAwesomeIcon className='techStackIcon' color="#f0db4f" icon={faSquareJs} />
+          <FontAwesomeIcon className='techStackIcon' color="#61DBFB" icon={faReact} />
         </div>
 
       </main>
