f997eacb8a66e649bc15f9ea096cc91f9a22aaca
Nicolai Harms <nicolai.harms@gmail.com>
2023-09-12 23:53:08 +0200

    lol thats me?

---
+++ src/components/ChaningText.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './ChaningText.css';
+class ChaningText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phrases: ['Kollega', 'Medarbejder', 'Front-end developer', 'Ven'],
+      currentIndex: 0,
+      animationTime: 3000,
+    };
+  }
+
+  componentDidMount() {
+    this.randomizeText();
+    this.animationInterval = setInterval(this.randomizeText, this.state.animationTime);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.animationInterval);
+  }
+
+  randomizeText = () => {
+    const { phrases, currentIndex } = this.state;
+    let newIndex = currentIndex;
+
+    do {
+      newIndex = this.randomNum(phrases.length);
+    } while (newIndex === currentIndex);
+
+    this.setState({ currentIndex: newIndex }, () => {
+      this.updateAnimationTime();
+    });
+  };
+
+  randomNum = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  updateAnimationTime = () => {
+    const phrase = document.querySelector('.random-word');
+    const compStyles = window.getComputedStyle(phrase);
+    let animation = compStyles.getPropertyValue('animation');
+
+    // Firefox support for non-shorthand property
+    if (animation === '') {
+      animation = compStyles.getPropertyValue('-moz-animation-duration');
+    }
+
+    // Webkit support for non-shorthand property
+    if (animation === '') {
+      animation = compStyles.getPropertyValue('-webkit-animation-duration');
+    }
+
+    const animationTime = parseFloat(animation.match(/\d*[.]?\d+/)) * 1000;
+    this.setState({ animationTime });
+  };
+
+  render() {
+    const { phrases, currentIndex } = this.state;
+    return (
+        <div className='randomTextWrapper'>
+        <h2>
+          Jeg vil gerne være din nye <span className='random-word'>{phrases[currentIndex]}</span>
+        </h2>
+        </div>
+    );
+  }
+}
+
+export default ChaningText;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HovereffectProjects from '../components/HovereffectsProjects.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faSquareJs, faReact, faCss3Alt, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import BlobSvg from '../components/BlobSvg.jsx';
+import ChaningText from '../components/ChaningText.jsx';
 
 
 export default function Home() {
@@ -16,8 +17,8 @@ export default function Home() {
    
       <h3>Front-End Developer med passion for inaktive løsninger og design</h3>
       <div className='soMe'>
-        <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
-        <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+        <a href="#"><FontAwesomeIcon color='white' icon={faGithub} /></a>
+        <a href="#"><FontAwesomeIcon color='white' icon={faLinkedin} /></a>
       </div>
     </div>
   </div>
@@ -31,8 +32,15 @@ export default function Home() {
           <FontAwesomeIcon className='techStackIcon' color="#2965f1" icon={faCss3Alt} />
           <FontAwesomeIcon className='techStackIcon' color="#f0db4f" icon={faSquareJs} />
           <FontAwesomeIcon className='techStackIcon' color="#61DBFB" icon={faReact} />
+          <hr className='techHr' />
         </div>
-
+        <div className='aboutMe'>
+       <img className='secondProfilePicture' src="src\assets\placeholderprofile.jpg" alt="" />
+        <div className="aboutMeText">
+        <ChaningText />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dignissimos et quia quis est sed nihil quas, ea deleniti enim dolorum ab libero, maxime placeat porro autem, iusto saepe sequi?</p>
+        </div>
+        </div>
       </main>
       <HovereffectProjects />
       {/* More content */}
