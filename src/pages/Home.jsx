bf8a543919cc6bc308434631aba737d66334b0b7
Nicolai Harms <nicolai.harms@gmail.com>
2023-09-15 00:01:10 +0200

    adjustments - desktop almost finished - Just need burgermenu and react solutions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HovereffectProjects from '../components/HovereffectsProjects.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faSquareJs, faReact, faCss3Alt, faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faSquareJs, faReact, faCss3Alt, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 import aboutMePicture from '../assets/aboutmepicture.png';
 import contactpicture from '../assets/contactmepicture.webp';
@@ -15,22 +15,22 @@ import ChaningText from '../components/ChaningText.jsx';
 export default function Home() {
   return (
     <div className='siteWrapper'>
-  
-  <main className="mainWrapper">
-      <div className='textToPictureWrapper'>
-  <div className='textWrapperMain'>
-  <div className='headerContainer'>
-    <h2>Hej, jeg er Nicolai!</h2>
-   
-      <h3>Front-End Developer med passion for inaktive løsninger og design</h3>
-      <div className='soMe'>
-        <a className='github' href="#"><FontAwesomeIcon  icon={faGithub} /></a>
-        <a className='linkedin' href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
-      </div>
-    </div>
-  </div>
-  <BlobSvg />
-</div>
+
+      <main className="mainWrapper">
+        <div className='textToPictureWrapper'>
+          <div className='textWrapperMain'>
+            <div className='headerContainer'>
+              <h2>Hej, jeg er Nicolai!</h2>
+
+              <h3>Front-End Developer med passion for inaktive løsninger og design</h3>
+              <div className='soMe'>
+                <a className='github' href="#"><FontAwesomeIcon icon={faGithub} /></a>
+                <a className='linkedin' href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+              </div>
+            </div>
+          </div>
+          <BlobSvg />
+        </div>
 
         <div className='techStack'>
           <h5 className='techHeader'>Tech Stack</h5>
@@ -39,46 +39,61 @@ export default function Home() {
           <FontAwesomeIcon className='techStackIcon' color="#2965f1" icon={faCss3Alt} />
           <FontAwesomeIcon className='techStackIcon' color="#f0db4f" icon={faSquareJs} />
           <FontAwesomeIcon className='techStackIcon' color="#61DBFB" icon={faReact} />
-          
+
           <hr className='techHr' />
         </div>
         <ChaningText />
-  <div className='aboutMeWrapper' style={{ backgroundImage: `url(${aboutMePicture})` }}>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dignissimos et quia quis est sed nihil quas, ea deleniti enim dolorum ab libero, maxime placeat porro autem, iusto saepe sequi?
-  </p>
-    {/* You can add any additional styling here */}
-  </div>
-  
+        <div className='aboutMeWrapper' style={{ backgroundImage: `url(${aboutMePicture})` }}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dignissimos et quia quis est sed nihil quas, ea deleniti enim dolorum ab libero, maxime placeat porro autem, iusto saepe sequi?
+          </p>
+          {/* You can add any additional styling here */}
+        </div>
 
 
-        
+
+
       </main>
-   
+
 
       <HovereffectProjects />
-      <div>
-      
+      <div className='backgroundColorWrapper'>
+
         <section className='contactMeWrapper'>
+
           <article className='contactMeText'>
             <h2>Contact me</h2>
-          <h3>If you have any questions, or want to work with me, do not hesitate to contact me.</h3>
-            <div className='contactMeBoxes'>
-            <a href="#"><FontAwesomeIcon  icon={faPhone} /></a>
-              <p>+45 61 72 03 11</p>
-            </div>
-            <div className='contactMeBoxes'>
-            <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
+            <h3>If you have any questions, or want to work with me, do not hesitate to contact me.</h3>
+            <div className='iconWrapper'>
+  <div className='contactMeBoxes'>
+    <a href="#" className='contactLink'>
+      <FontAwesomeIcon className='contactIcons' icon={faPhone} />
+      <p className='contactText'>+45 61 72 03 11</p>
+    </a>
+  </div>
+  <div className='contactMeBoxes'>
+    <a href="#" className='contactLink'>
+      <FontAwesomeIcon className='contactIcons' icon={faEnvelope} />
+      <p className='contactText'>Nicolai.harms@gmail.com</p>
+    </a>
+  </div>
+  <div className='contactMeBoxes'>
+    <a href="#" className='contactLink'>
+      <FontAwesomeIcon className='contactIcons' icon={faLinkedin} />
+      <p className='contactText'>Nicolai Harms</p>
+    </a>
+  </div>
+</div>
 
-              <p>Nicolai.harms@gmail.com</p>
-            </div>
-            <div className='contactMeBoxes'>
-              <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
-              <p>Nicolai Harms</p>
-            </div>
+
           </article>
           <img className='contactMeImage' src={contactpicture} alt="" width="100%" height="100%" />
         </section>
+     
+      <footer className='footerWrapper'>
+        <p>© 2021 Nicolai Harms</p>
+        <a href="">Back to top</a>
+      </footer>
       </div>
     </div>
   );
